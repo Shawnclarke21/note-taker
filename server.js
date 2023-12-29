b3862cc4cae4b77c94bbb3a13a3201c1a6035084
unknown <srclarke777@gmail.com>
2023-12-29 13:14:45 -0700

    put in my code to get the server up an running in the server.js file

--- server.js
+++ server.js
@@ -3,3 +3,13 @@ const app=express();
 const PORT =process.env.PORT ||3001;
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes =require('./routes/htmlRoutes');
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(apiRoutes)
+app.use(htmlRoutes)
+
+app.listen(PORT,()=>{
+    console.log(`API server now on port ${PORT}`)
+});
